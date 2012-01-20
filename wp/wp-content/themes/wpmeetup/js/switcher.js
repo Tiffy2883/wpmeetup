93d74ee0af4931930eafb2e5be942704f6d0eadf
Eric Teubert <ericteubert@googlemail.com>
2012-01-20 11:57:05 +0100

    show upcoming meetups on germany map
    issue #2

--- wp/wp-content/themes/wpmeetup/js/switcher.js
+++ wp/wp-content/themes/wpmeetup/js/switcher.js
@@ -4,7 +4,7 @@ jQuery.noConflict();
 		init : function () {
 			
 			// Single Map
-			if ( $( '#single_map' ) ) {
+			if ( $( '#single_map' ).length ) {
 				if ( $( '#single_map' ).data( 'geo-lat' ) != undefined && $( '#single_map' ).data( 'geo-lng' ) != undefined ) {
 					var lat = $( '#single_map' ).data( 'geo-lat' );
 					var lng = $( '#single_map' ).data( 'geo-lng' );
@@ -35,13 +35,64 @@ jQuery.noConflict();
 				var marker = new google.maps.Marker( {
 					position: marker_point,
 					map: map,
-					icon: switcher_vars.template_dir + '/images/pin.png',
-					title: "Hello World"
+					icon: switcher_vars.template_dir + '/images/pin.png'
 				} );
 				
 				google.maps.event.addListener(marker, 'click', function() {
 			      infowindow.open( map, marker );
 			    } );
+			}
+			
+		    // Multi Map
+			if ( $( '#map' ).length ) {
+				
+				var germany = new google.maps.LatLng( 51.133333, 10.416667 );
+				var map_options = {
+					zoom: 6,
+					center: germany,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				
+				var map = new google.maps.Map( $( '#map' )[ 0 ], map_options );
+				$( '#map_data .meetup_event' ).each( function( index, value ) {
+					var meetup = $( value );
+					var meetup_data = meetup.data();
+					if ( meetup_data.geoLat != undefined && meetup_data.geoLng  != undefined ) {
+						var lat = meetup_data.geoLat;
+						var lng = meetup_data.geoLng;
+						var zoom = 10;
+					}
+					else {
+						var lat = 51.133333;
+						var lng = 10.416667;
+						var zoom = 6;
+					}
+					
+					var marker_point = new google.maps.LatLng( lat, lng );
+					var location_url = (meetup_data.location_url) ? "<a href=\"" + meetup_data.location_url + "\">" + meetup_data.location_url + "</a>" : "";
+					var info_window_content = ''
+						+ meetup_data.location + "</br>"
+						+ location_url + "</br>"
+						+ meetup_data.street + " " + meetup_data.number + "</br>"
+						+ meetup_data.plz + " " + meetup_data.town + "</br>"
+						+ "<strong>" + meetup_data.date + " " + meetup_data.time + "</strong>" + "</br>"
+					;
+
+					$("#marker_address").hide();
+					var infowindow = new google.maps.InfoWindow({
+				        content: info_window_content
+				    });
+
+					var marker = new google.maps.Marker( {
+						position: marker_point,
+						map: map,
+						icon: switcher_vars.template_dir + '/images/pin.png'
+					} );
+
+					google.maps.event.addListener(marker, 'click', function() {
+				      infowindow.open( map, marker );
+				    } );
+				} );
 			}
 		},
 	};
